b395f888267a68b87120d6ddd776b21a1534cf6d
Athos Cordeiro Rosa <athoscordeiro@hotmail.com>
2026-05-14 19:08:00 -0400

    c
    c

--- app/helpers/settings.php
+++ app/helpers/settings.php
@@ -2,8 +2,12 @@
 
 declare(strict_types=1);
 
+session_start();
+
 define('ROOT', dirname(__FILE__, 3));
 #DIRETÓRIO DAS VIEWS
 define('DIR_VIEWS', ROOT . '/app/view');
 #EXTENSÃO PADRÃO DAS VIEWS
 define('EXT_VIEWS', '.html');
+#Chave secreta para geração de tokens
+define('SECRET_KEY', '2aa44e53-0c02-48ad-98f1-167fabb5cd67');
